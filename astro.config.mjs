addff6b6f3f0479e553538ca48596692a7c77c25
Gonzalo Oviedo L <goviedo.sevenit@gmail.com>
2024-06-14 02:22:07 -0400

    Mejoras para mayor facilidad de uso

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	// Define your routes here
-	routes: [
-		{ path: '/', component: './src/pages/index.astro' },
-		{ path: '/about', component: './src/pages/about.astro' },
-		{ path: '/videos/:id', component: './src/pages/video.astro' },
-	  ],
 });
